b7dd668c55287de19a4a164db8e9d2bb6bd22877
chrisalexander <chris@chris-alexander.co.uk>
2020-03-14 16:23:36 +0000

    Initial pfd driver

---
+++ README.md
@@ -0,0 +1,26 @@
+# pfd-driver
+
+PiFaceDigital driver
+
+This device can't handle multiple executables connecting to it at the same time,
+so we use one service and then sub-services use this as a lower level API.
+
+## Message Spec
+
+Format: \<channel> "message"
+
+**Inputs**
+
+* \<pfd.inputs> *
+  * Triggers a request for status of all of the inputs
+
+**Outputs**
+
+* \<pfd.input.[0:7]> "input.[0:7].on"
+  * The input at index [0:7] has been connected
+  * Triggered automatically
+  * Also sent when an input is provided on \<pfd.inputs> channel, if the input is on
+* \<pfd.input.[0:7]> "input.[0:7].off"
+  * The input at index [0:7] has been disconnected
+  * Triggered automatically
+  * Also sent when an input is provided on \<pfd.inputs> channel, if the input is off
---
+++ pfd-driver.py
@@ -0,0 +1,42 @@
+if __name__ == '__main__':
+    import systemd.daemon
+    import pifacedigitalio
+    import redis
+    import time
+
+    print('Startup')
+    pfd = pifacedigitalio.PiFaceDigital()
+    listener = pifacedigitalio.InputEventListener(chip=pfd)
+    r = redis.Redis(host='192.168.0.1', port=6379, db=0)
+    p = r.pubsub(ignore_subscribe_messages=True)
+    p.subscribe('pfd.inputs')
+    print('Startup complete')
+    systemd.daemon.notify('READY=1')
+
+    input_on = list()
+    input_off = list()
+
+    try:
+        for i in range(0, 8):
+            input_on[i] = lambda e: r.publish(
+                "pfd.input." + str(i), "input." + str(i) + ".on")
+            input_on[i] = lambda e: r.publish(
+                "pfd.input." + str(i), "input." + str(i) + ".off")
+            listener.register(
+                i, pifacedigitalio.IODIR_FALLING_EDGE, input_on[i])
+            listener.register(
+                i, pifacedigitalio.IODIR_RISING_EDGE, input_off[i])
+
+        listener.activate()
+
+        for message in p.listen():
+            # If message is received, send current status
+            for i in range(0, 8):
+                if pfd.input_pins[i].value > 0:
+                    (input_on[i])(None)
+                else:
+                    (input_off[i])(None)
+    except:
+        listener.deactivate()
+        pfd.deinit_board()
+        print("Goodbye")
